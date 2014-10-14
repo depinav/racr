613874e06c976c45c39d004c450d6175fba5b46c
pbrejcha <peter.brejcha@gmail.com>
2014-10-13 19:23:54 -0500

    updating indentation because it was ugly.

--- app/controllers/raceController.js
+++ app/controllers/raceController.js
@@ -2,88 +2,88 @@ var Race    = require('../models/race');
 
 module.exports = function(app) {
 
-  // Get all races
-  app.get('/api/races', function(req, res) {
+    // Get all races
+    app.get('/api/races', function(req, res) {
 
-    Race.find(function(err, races) {
+        Race.find(function(err, races) {
 
-      sendErr(res, err);
+            sendErr(res, err);
 
-      res.json(races);
+            res.json(races);
+        });
     });
-  });
 
-  // Get single race
-  app.get('/api/races/:race_id', function(req, res) {
+    // Get single race
+    app.get('/api/races/:race_id', function(req, res) {
 
-    Race.find({ _id : req.params.race_id }, function(err, race) {
+        Race.find({ _id : req.params.race_id }, function(err, race) {
 
-      sendErr(err);
+            sendErr(err);
 
-      res.json(race);
+            res.json(race);
+        });
     });
-  });
 
-  // Create a new race
-  app.post('/api/races', function(req, res) {
+    // Create a new race
+    app.post('/api/races', function(req, res) {
 
-    var bdy = req.body;
+        var bdy = req.body;
 
-    Race.create(
-    { 
-      name : bdy.name,
-      type : bdy.type,
-      date : bdy.date
-    },
-    function(err, race) {
+        Race.create(
+            {
+                name : bdy.name,
+                type : bdy.type,
+                date : bdy.date
+            },
+            function(err, race) {
 
-      sendErr(res, err);
+                sendErr(res, err);
 
-      Race.find(function(err, races) {
-        
-        sendErr(res, err);
+                Race.find(function(err, races) {
 
-        res.json(races);
-      });
+                    sendErr(res, err);
+
+                    res.json(races);
+                });
+            });
     });
-  });
 
-  // Delete a race
-  app.delete('/api/races/:race_id', function(req, res) {
+    // Delete a race
+    app.delete('/api/races/:race_id', function(req, res) {
 
-    Race.remove({ _id : req.params.race_id }, function(err, race) {
+        Race.remove({ _id : req.params.race_id }, function(err, race) {
 
-      sendErr(res, err);
+            sendErr(res, err);
 
-      Race.find(function(err, races) {
+            Race.find(function(err, races) {
 
-        sendErr(err);
+                sendErr(err);
 
-        res.json(races);
-      });
+                res.json(races);
+            });
+        });
     });
-  });
 
-  app.get('/api/races/drop', function(req, res) {
+    app.get('/api/races/drop', function(req, res) {
 
-    Race.collection.drop(function(err) {
+        Race.collection.drop(function(err) {
 
-      sendErr(err);
+            sendErr(err);
 
-      console.log("Collection Dropped");
-      res.send('Collection Successfully dropped.');
+            console.log("Collection Dropped");
+            res.send('Collection Successfully dropped.');
+        });
     });
-  });
 
-  // Home route
-  app.get('/', function(req, res) {
+    // Home route
+    app.get('/', function(req, res) {
 
-    res.sendfile('./public/pages/main.html');
-  });
+        res.sendfile('./public/pages/main.html');
+    });
 }
 
 sendErr = function(res, err) {
 
-  if(err)
-    res.send(err);
+    if(err)
+        res.send(err);
 }
